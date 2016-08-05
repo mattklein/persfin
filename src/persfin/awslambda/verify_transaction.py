e99154c508a3bc674ae28228e4cb0013ce9b1224
Matt Klein <mpklein@gmail.com>
2016-08-05 17:26:34 -0400

    Switch to using GET instead of POST due to iOS weirdness
    Switch to using GET instead of POST due to iOS weirdness

--- src/persfin/awslambda/verify_transaction.py
+++ src/persfin/awslambda/verify_transaction.py
@@ -8,9 +8,24 @@ def lambda_handler(event, context):
     logging.getLogger().setLevel(logging.INFO)
     logging.debug('Received event:\n%s', event)
 
-    verify_transaction(int(event['verif-attempt-id']),
-                       event['verified'] == 'Yes',
-                       int(event['forward-to']),
-                       int(event['attributed-to']))
+    try:
+        # This handles the case where we're receiving an event via a POST request (which I'm not currently
+        # using -- see background/explanation in transaction_verification.html)
+        verif_attempt_id = int(event['verif-attempt-id'])
+        verified = event['verified'] == 'Yes'
+        forward_to = int(event['forward-to'])
+        attributed_to = int(event['attributed-to'])
+    except KeyError:
+        try:
+            # This handles the case where we're receiving an event via a GET request
+            verif_attempt_id = int(event['params']['querystring']['verif-attempt-id'])
+            verified = event['params']['querystring']['verified'] == 'Yes'
+            forward_to = int(event['params']['querystring']['forward-to'])
+            attributed_to = int(event['params']['querystring']['attributed-to'])
+        except KeyError:
+            logging.error("Couldn't get parameters from event:\n%s", event)
+            raise
+
+    verify_transaction(verif_attempt_id, verified, forward_to, attributed_to)
 
     return "OK"
