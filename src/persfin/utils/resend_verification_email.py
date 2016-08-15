ae377802e38f92b0e0600cbb25f844a11491deb0
Matt Klein <mpklein@gmail.com>
2016-08-15 08:44:11 -0400

    Handle list of verif_attempt_ids instead of just one
    Handle list of verif_attempt_ids instead of just one

--- src/persfin/utils/resend_verification_email.py
+++ src/persfin/utils/resend_verification_email.py
@@ -8,43 +8,44 @@ from persfin.core.transaction_verification import send_verification_email, get_p
 from persfin.db import engine, verification_attempt_tbl, transaction_tbl, account_tbl
 
 
-def resend_verification_email(verif_attempt_id):
+def resend_verification_email(verif_attempt_ids):
 
     db_conn = engine.connect()
-    s = select([verification_attempt_tbl.c.id,
-                verification_attempt_tbl.c.asked_of,
-                transaction_tbl.c.date,
-                transaction_tbl.c.amount,
-                transaction_tbl.c.merchant,
-                account_tbl.c.name.label('account_name'),
-                ]) \
-            .select_from(verification_attempt_tbl
-                .join(transaction_tbl)
-                .join(account_tbl)) \
-            .where(verification_attempt_tbl.c.id == verif_attempt_id)
+    for verif_attempt_id in verif_attempt_ids:
+        s = select([verification_attempt_tbl.c.id,
+                    verification_attempt_tbl.c.asked_of,
+                    transaction_tbl.c.date,
+                    transaction_tbl.c.amount,
+                    transaction_tbl.c.merchant,
+                    account_tbl.c.name.label('account_name'),
+                    ]) \
+                .select_from(verification_attempt_tbl
+                    .join(transaction_tbl)
+                    .join(account_tbl)) \
+                .where(verification_attempt_tbl.c.id == verif_attempt_id)
 
-    rs = db_conn.execute(s)
-    assert rs.rowcount == 1
-    dbrow = rs.fetchone()
+        rs = db_conn.execute(s)
+        assert rs.rowcount == 1
+        dbrow = rs.fetchone()
 
-    verifier = get_user_by_id(db_conn, dbrow['asked_of'])
-    possible_attributed_tos = get_possible_attributed_to_users(db_conn)
-    possible_other_verifiers = get_possible_forward_to_users(db_conn, [verifier.id, ])
+        verifier = get_user_by_id(db_conn, dbrow['asked_of'])
+        possible_attributed_tos = get_possible_attributed_to_users(db_conn)
+        possible_other_verifiers = get_possible_forward_to_users(db_conn, [verifier.id, ])
 
-    send_verification_email(
-        dbrow['id'],
-        verifier,
-        dbrow['account_name'],
-        dbrow['date'],
-        dbrow['amount'],
-        dbrow['merchant'],
-        possible_attributed_tos,
-        possible_other_verifiers)
+        send_verification_email(
+            dbrow['id'],
+            verifier,
+            dbrow['account_name'],
+            dbrow['date'],
+            dbrow['amount'],
+            dbrow['merchant'],
+            possible_attributed_tos,
+            possible_other_verifiers)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print 'Usage: %s <verif_attempt_id>' % sys.argv[0]
+    if len(sys.argv) < 2:
+        print 'Usage: %s <verif_attempt_ids>' % sys.argv[0]
         sys.exit(1)
     logging.getLogger().setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s',
@@ -52,5 +53,5 @@ if __name__ == '__main__':
     stream_handler = logging.StreamHandler(sys.stdout)
     stream_handler.setFormatter(formatter)
     logging.getLogger().addHandler(stream_handler)
-    verif_attempt_id = int(sys.argv[1])
-    resend_verification_email(verif_attempt_id)
+    verif_attempt_ids = [int(id_) for id_ in sys.argv[1:]]
+    resend_verification_email(verif_attempt_ids)
