6efab199fbab87e027c523719d4f8521aa281dd2
Matt Klein <mpklein@gmail.com>
2016-08-16 13:20:13 -0400

    Logging
    Logging

--- src/persfin/utils/move_to_folder.py
+++ src/persfin/utils/move_to_folder.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 import boto
@@ -7,6 +8,7 @@ from persfin import S3_BUCKET_NAME
 
 
 def move_to_folder(message_id, old_folder_name, new_folder_name):
+    logging.info('Moving %s from %s to %s', message_id, old_folder_name, new_folder_name)
     s3_conn = boto.connect_s3(s3_full.ACCESS_KEY_ID, s3_full.SECRET_ACCESS_KEY)
     bucket = s3_conn.get_bucket(S3_BUCKET_NAME)
     k = bucket.get_key('%s/%s' % (old_folder_name, message_id))
@@ -15,6 +17,12 @@ def move_to_folder(message_id, old_folder_name, new_folder_name):
 
 
 if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    logging.getLogger().addHandler(stream_handler)
     if len(sys.argv) != 4:
         print 'Usage: %s message_id from_folder to_folder' % sys.argv[0]
         print 'E.g.:  %s bms9ienlmp6q9p2952o6up34kd12g9su34oa5701 failed should-not-process' % sys.argv[0]
