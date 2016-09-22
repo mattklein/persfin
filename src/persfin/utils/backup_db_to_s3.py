752c49a07603a0ec0b0f469218157883d87abae9
Matt Klein <mpklein@gmail.com>
2016-09-21 22:25:30 -0400

    Add logging
    Add logging

--- src/persfin/utils/backup_db_to_s3.py
+++ src/persfin/utils/backup_db_to_s3.py
@@ -1,21 +1,24 @@
 from datetime import datetime
+import logging
 import os
 import subprocess
 
 import boto3
 
 from credentials import s3_full
-from persfin import DB_BACKUP_BUCKET_NAME
+from persfin import DB_BACKUP_BUCKET_NAME, configure_logging_file
 
 
 def main():
+    logging.info('Beginning backup_db_to_s3.py')
     now = datetime.utcnow()
     output_dir = '/tmp'
     output_filename = 'pg_dumpall_%s.gz' % now.strftime('%Y%m%d')
     output_fullpath = os.path.join(output_dir, output_filename)
-    subprocess.check_call(
-        'pg_dumpall -U postgres -h localhost -p 5432 | gzip -c > %s' % output_fullpath,
-        shell=True)
+    cmd = 'pg_dumpall -U postgres -h localhost -p 5432 | gzip -c > %s' % output_fullpath
+    logging.info('Running dump with command "%s"', cmd)
+    subprocess.check_call(cmd, shell=True)
+    logging.info('Uploading output file to S3')
     s3 = boto3.client('s3',
                       aws_access_key_id=s3_full.ACCESS_KEY_ID,
                       aws_secret_access_key=s3_full.SECRET_ACCESS_KEY)
@@ -23,8 +26,11 @@ def main():
                   Key=output_filename,
                   Body=open(output_fullpath, 'rb'),
                   ContentType='application/gzip')
+    logging.info('Removing %s', output_fullpath)
     os.remove(output_fullpath)
+    logging.info('Done')
 
 
 if __name__ == '__main__':
+    configure_logging_file('/var/log/backup_db_to_s3.log')
     main()
